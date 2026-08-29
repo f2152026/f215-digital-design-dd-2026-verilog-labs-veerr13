29a1959b48afeda5b8c24dab0caaa56780bfea67
Veer Raghuvanshi <232750757+VeerR13@users.noreply.github.com>
2026-08-29 14:16:21 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -28,10 +28,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
  
-  xor #(2,3) (ps,   a,   b);
-  and #(2,3) (pc1,  a,   b);
-  xor #(2,3) (sum,  cin, ps);
-  and #(2,3) (pc2,  cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
+  xor #(3,2) (ps,   a,   b);
+  and #(3,2) (pc1,  a,   b);
+  xor #(3,2) (sum,  cin, ps);
+  and #(3,2) (pc2,  cin, ps);
+  or  #(3,2) (cout, pc1, pc2);
  
 endmodule
