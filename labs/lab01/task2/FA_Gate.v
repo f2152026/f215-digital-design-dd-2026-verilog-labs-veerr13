dc57aa9556c3214c9d3430b24ea8af5d62d7fbd9
Veer Raghuvanshi <232750757+VeerR13@users.noreply.github.com>
2026-08-29 09:56:19 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -17,6 +17,8 @@
 // gates are rarely symmetric this way. Re-simulate with the SAME
 // ripple_adder.v and tb.v; nothing else needs to change.
 
+`timescale 1ns/1ps
+ 
 module FA_Gate(
   input  a,
   input  b,
@@ -25,11 +27,11 @@ module FA_Gate(
   output cout
 );
   wire ps, pc1, pc2;
-
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
-
+ 
+  xor #(2,3) (ps,   a,   b);
+  and #(2,3) (pc1,  a,   b);
+  xor #(2,3) (sum,  cin, ps);
+  and #(2,3) (pc2,  cin, ps);
+  or  #(2,3) (cout, pc1, pc2);
+ 
 endmodule
