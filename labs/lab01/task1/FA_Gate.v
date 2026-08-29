7bf7edd27c7802c4a64f56e5055979c6aa2772ef
Veer Raghuvanshi <raghuvanshi.veer004@gmail.com>
2026-08-29 09:00:20 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -7,6 +7,8 @@
 //           instantiations below into any different sequence, then
 //           re-simulate with the same tb.v and compare.
 
+`timescale 1ns/1ps
+
 module FA_Gate(
   input  a,
   input  b,
@@ -16,10 +18,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  xor #(2) (ps,   a,   b);
+  and #(2) (pc1,  a,   b);
+  xor #(2) (sum,  cin, ps);
+  and #(2) (pc2,  cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
