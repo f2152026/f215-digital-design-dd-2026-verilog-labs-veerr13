a9b9572e83b59c5e8ba79b5bc6e8dbb07cb464f9
Veer Raghuvanshi <232750757+VeerR13@users.noreply.github.com>
2026-09-19 08:14:49 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,13 +1,13 @@
-// tb.v
-// Starter testbench template -- YOU complete this file.
-
 module tb;
 
-  // TODO: declare the inputs and outputs
+  reg  [1:0] sel;
+  wire [7:0] dout;
 
-  // TODO: instantiate DUT here
+  lut DUT (
+    .sel  (sel),
+    .dout (dout)
+  );
 
-  // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
@@ -17,11 +17,14 @@ module tb;
   end
 
   initial begin
-    // TODO: apply different input combinations
-
+    sel = 0;
+    #5  sel = 1;
+    #5  sel = 2;
+    #5  sel = 3;
+    #5  $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%0d | dout=%0d", sel, dout);
 
 endmodule
